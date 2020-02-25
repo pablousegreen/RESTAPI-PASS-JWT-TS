b4dc4ed8a07d799827dbe8d2cd74092148ae93a0
Pablousegreen <pablousegreenj2ee@gmail.com>
2020-02-24 21:51:38 -0600

    001 base

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express'
+import passport from 'passport'
+import cors from 'cors'
+import morgan from 'morgan'
+
+const app = express();
+
+//settings 
+app.set('port', process.env.PORT || 3001);
+
+//middlewares
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(passport.initialize());
+//passport.use()
+
+
+app.get('/', (req, res)=>{
+    return res.send((`The API is at http://localhost:${app.get('port')}}`);
+})
+
+export default app;
---
+++ src/database.ts
@@ -0,0 +1,1 @@
+import mongoose, {CollationOptions} from 'mongoose';
